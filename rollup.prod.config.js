5416bf971fd09fb2b9281df524eced678bedb126
hewei <hwjf123456@sina.cn>
2020-08-16 13:27:13 +0800

    2020-08-16(13:27)

---
+++ rollup.prod.config.js
@@ -0,0 +1,13 @@
+// Rollup plugins.
+import replace from 'rollup-plugin-replace'
+import { uglify } from 'rollup-plugin-uglify'
+
+// Import the development configuration.
+import config from './rollup.config'
+
+// Inject the production settings.
+config.output.file = 'build/app.min.js'
+config.plugins[5] = replace({ 'process.env.NODE_ENV': JSON.stringify('production') })
+config.plugins.push(uglify())
+
+export default config
