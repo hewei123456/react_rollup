2aa7b7146433e967d340e1f0114bb62ef868d738
hewei <hewei95324@offcn.com>
2020-08-17 12:44:47 +0800

    '2020-08-17(12:44)'

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -6,7 +6,6 @@ import { terser } from 'rollup-plugin-terser'
 import config from './rollup.config'
 
 // Inject the production settings.
-config.output.file = 'build/app.min.js'
 config.plugins[0] = replace({ 'process.env.NODE_ENV': JSON.stringify('production') })
 config.plugins.push(terser())
 
