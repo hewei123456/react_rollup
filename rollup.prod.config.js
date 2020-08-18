e5a0354bee31b9aa58478c0f621f956a07049549
hewei <hewei95324@offcn.com>
2020-08-18 09:13:32 +0800

    '2020-08-18(9:13)'

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -3,7 +3,7 @@ import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
 // Import the development configuration.
-import config from './rollup.config'
+import config from './rollup.dev.config'
 
 // Inject the production settings.
 config.plugins[0] = replace({ 'process.env.NODE_ENV': JSON.stringify('production') })
